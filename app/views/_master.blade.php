e49b5b6766c232451215298e3ecacaec6ba45ac9
Jarek Frackowiak <jarektf@gmail.com>
2016-11-08 10:02:26 -0600

    Updates to run latest versions of jQuery, jQuery UI, Bootstrap, and Bootswatch theme.

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -7,13 +7,16 @@
 	<meta name="keywords" content="SBOT new hire portal">
 	<meta name="description" content="SBOT New Hire Portal">
 	<!-- Get jquery from CDN -->
-	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.2/themes/humanity/jquery-ui.css">
-	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/humanity/jquery-ui.css">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 	<!-- Get Bootstrap from CDN -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<!-- Get Bootswatch theme from CDN -->
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.2.0/readable/bootstrap.min.css">
+	<!--
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css">
+	-->
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css">
 	<!-- Get local CSS -->
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
 
