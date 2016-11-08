1f69ee508679f4373407bdf42e204a1aa4f927d4
Jarek Frackowiak <jarektf@gmail.com>
2016-11-08 15:08:22 -0600

    Added Austin skyline background image and CSS3 styles.

--- app/views/user_login.blade.php
+++ app/views/user_login.blade.php
@@ -10,17 +10,23 @@
     <div class='error'>{{ $message }}</div>
 @endforeach
 
-<h4>Please login to update your profile and submit forms.</h4><br>
+<h3>Please login to complete your new hire forms.</h3><br>
 
 {{ Form::open(array('url' => '/user/login')) }}
 
-    {{ Form::label('username:') }}
-    {{ Form::text('username') }}<br><br>
+	<h4>
+	<div class='form-group row'>
+	    {{ Form::label('username', 'Username', 'class=col-xs-1 col-form-label') }}
+	    {{ Form::text('username') }}<br><br>
 
-    {{ Form::label('password:') }}
-    {{ Form::password('password') }}<br><br>
+	    {{ Form::label('password', 'Password', 'class=col-xs-1 col-form-label') }}
+	    {{ Form::password('password') }}<br><br><br>
 
-    {{ Form::submit('Log in', array('class' => 'btn btn-primary')) }}
-
+	    {{ Form::submit('Log in', array('class' => 'btn btn-primary')) }}
+	</div>
+	</h4>
+	<br>
+	* If you have any questions or need assistance, please contact Human Resources at 1-800-204-2222 ext. 1489 or email
+	<a href="mailto:employment@texasbar.com" target="_blank">employment@texasbar.com</a>.
 {{ Form::close() }}
 @stop
