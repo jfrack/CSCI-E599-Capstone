cdab285e9da2445746663b95fc0dd23da2d51e35
Jarek Frackowiak <jarektf@gmail.com>
2016-11-23 13:36:18 -0600

    Updates to checklist_employee database model.

--- app/models/Checklist_Employee.php
+++ app/models/Checklist_Employee.php
@@ -9,10 +9,13 @@ class Checklist_Employee extends Eloquent {
 	 *
 	 * @var string
 	 */
-	protected $table = 'checklist_emplooyee';
+	protected $table = 'checklist_employee';
 
 	# The guarded properties specifies which attributes should *not* be mass-assignable
 	protected $guarded = array('created_at', 'updated_at');
+
+	# Enable fillable so we can use the Model shortcut Create
+	protected $fillable = array('employee_id', 'checklist_id', 'status', 'comments');
 
 	/**
 	* Execute raw SQL query
