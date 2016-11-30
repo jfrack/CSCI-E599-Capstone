230b188c94996108d713b299646ca2d144c3e7ab
jfrack <jarektf@gmail.com>
2016-11-30 02:10:37 -0600

    Added new checklists manager delete view.

---
+++ app/views/employee_checklists_manager_delete.blade.php
@@ -0,0 +1,28 @@
+@extends('_master')
+
+@section('title')
+	Employee Checklists Manager Delete
+@stop
+
+@section('content')
+	@foreach($errors->all() as $message)
+	    <div class='error'>{{ $message }}</div>
+	@endforeach
+
+	<div class="display_box">
+		<h4><legend>{{ $checklist_item->name }}</legend></h4>
+		{{ $checklist_item->description }}
+		<br><br>
+		<div class='alert alert-danger'>
+			<span class="glyphicon glyphicon-exclamation-sign"></span>
+			Are you sure to delete checklist item for all employees?
+		</div>
+		<br>
+		{{ Form::open(array('url' => '/employee/checklists_manager/delete/$checklist_id')) }}
+			{{ Form::hidden('checklist_id', $checklist_item->id) }}
+		    {{ Form::submit('Delete Item', array('class' => 'btn btn-primary')) }}
+		    <a href=".." class="btn btn-warning">Cancel</a>
+		{{ Form::close() }}
+
+	</div>
+@stop
