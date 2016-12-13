01167a8a8e11defe7254ae72241d3a1987a96b58
jfrack <jarektf@gmail.com>
2016-12-12 21:39:23 -0600

    Added forms manager view.

---
+++ app/views/employee_forms_manager.blade.php
@@ -0,0 +1,66 @@
+@extends('_master')
+
+@section('title')
+	Employee Forms Manager
+@stop
+
+@section('content')
+	@foreach($errors->all() as $message)
+	    <div class='error'>{{ $message }}</div>
+	@endforeach
+
+{{ Form::open(array('url' => '/employee/forms_manager')) }}
+
+	<div class="display_box_wide">
+		<a href="" class="btn btn-primary">Add Form</a>
+		<br><br>
+
+		<table class="table table-bordered table-hover">
+			<th>Actions</th>
+			<th>Form</th>
+			<th>Description</th>
+			<th>Last Update</th>
+			<tr>
+				<td>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"> View</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"> Edit</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-save"> Download</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-print"> Print</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"> Delete</a>
+				</td>
+				<td>ERS Benefits Enrollment</td>
+				<td>Employee Retirement System yearly benefits enrollment package</td>
+				<td>12-10-2016 04:37pm</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"> View</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"> Edit</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-save"> Download</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-print"> Print</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"> Delete</a>
+				</td>
+				<td>IRS W-4</td>
+				<td>Federal employee's witholding allowance certificate</td>
+				<td>08-01-2015 10:14am</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"> View</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"> Edit</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-save"> Download</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-print"> Print</a>
+					<a href="" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"> Delete</a>
+				</td>
+				<td>Employee Handbook</td>
+				<td>State Bar of Texas employee handbook</td>
+				<td>10-19-2016 03:31pm</td>
+			</tr>
+		</table>
+
+		<br>
+		<a href="/" class="btn btn-primary">Back</a>
+	</div>
+
+{{ Form::close() }}	
+@stop
