d58a267c3bc4148594417ca469ab1e4423ef6062
Jarek Frackowiak <jarektf@gmail.com>
2016-12-15 12:11:29 -0600

    jQuery UI datepicker min and max date settings

--- public/js/NHPscripts.js
+++ public/js/NHPscripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 	$(function() {
 		$( '.datepicker' ).datepicker({
 			changeMonth: true,
-			changeYear: true
+			changeYear: true,
+			minDate: "-20Y", // datepicker calendar only goes back to 2006 max
+			maxDate: +365
 		});
 
 		$( '#employeeTable' ).DataTable();
